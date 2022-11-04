371fc20722b685d4ca26a7f39f559173cbcf362a
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-11-04 23:54:00 +0900

    PieChartコンポーネント

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Cards from "./features/covid/Cards/Cards";
 import Chart from "./features/covid/Chart/Chart";
+import PieChart from "./features/covid/PieChart/PieChart";
 
 function App() {
   return (
     <div className="App">
       <Cards />
       <Chart />
+      <PieChart />
     </div>
   );
 }
